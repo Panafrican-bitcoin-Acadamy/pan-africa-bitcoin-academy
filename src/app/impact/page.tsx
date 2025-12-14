5f8fa8e17b81562baf47aca65fed7101880efa04
joie199 <joieama2@gmail.com>
2025-12-14 07:33:45 -0500

    Add missing AnimatedSection import in impact page

--- src/app/impact/page.tsx
+++ src/app/impact/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useState, useRef } from 'react';
+import { AnimatedSection } from '@/components/AnimatedSection';
 
 interface ImpactMetrics {
   totalStudentsTrained: number;
